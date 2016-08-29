6885b5ce351ca312fed564d526f4466b96eb4ff8
luoyjx <yjk99@qq.com>
2016-08-29 23:47:34 +0800

    init commit

---
+++ example/app.js
@@ -0,0 +1,12 @@
+/**
+ * example - app.js
+ * @authors luoyjx (yjk99@qq.com)
+ * @date    2016-08-29 23:41:04
+ */
+
+var koa = require('koa')();
+var router = require('koa-router')();
+
+var koaRestMongoose = require('../lib/index');
+
+
---
+++ test/index.js
@@ -0,0 +1,6 @@
+/**
+ * test - index.js
+ * @authors luoyjx (yjk99@qq.com)
+ * @date    2016-08-29 22:37:50
+ */
+
